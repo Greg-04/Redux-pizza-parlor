70d4335ff04eb0fcda6c808739966a429a7428fa
Ying Her <yher.prime@gmail.com>
2024-02-03 12:44:23 -0600

    customer info fields created

---
+++ src/components/App/CustomerInfo/CustomerInfo.jsx
@@ -0,0 +1,14 @@
+function CustomerInfo() {
+  return (
+    <div>
+      <input>Name</input>
+      <input>Street Name</input>
+      <input>City</input>
+      <input>Zip</input>
+      <input type="checkbox">Pickup</input>
+      <input type="checkbox">Delivery</input>
+    </div>
+  );
+}
+
+export default CustomerInfo;
