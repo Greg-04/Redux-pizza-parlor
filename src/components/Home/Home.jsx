75c5471625f00ab4ece1ef376aa1504bb6a50127
Ying Her <yher.prime@gmail.com>
2024-02-10 08:23:07 -0600

    Home component added

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <h1 className="Home-title">Prime Pizza</h1>
+      </header>
+      {/*
+      <img src='images/pizza_photo.png' />
+      <p>Pizza is great.</p> */}
+    </div>
+  );
+}
+
+export default Home;
