6b16a4f1f468d97c746c11f408f2ca2d2584778f
Ying Her <yher.prime@gmail.com>
2024-02-10 08:00:20 -0600

    CustomerInfo form complete however, cannot view it in the client side until work with group on axios call and routing path

--- src/components/App/CustomerInfo/CustomerInfo.jsx
+++ src/components/App/CustomerInfo/CustomerInfo.jsx
@@ -1,13 +1,43 @@
+import { Button } from '@mui/material';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
 function CustomerInfo() {
+  const [inputName, setInputName] = useState('');
+  const [inputStreetName, setInputStreetName] = useState('');
+  const [inputCity, setInputCity] = useState('');
+  const [inputZip, setInputZip] = useState('');
+  const dispatch = useDispatch();
+
+  const handleClickNavToCheckout = (event) => {
+    event.preventDefault();
+    dispatch({
+      type: 'CUSTOMER_INFO_ADD',
+      payload: {
+        customer_name: inputName,
+        street_address: inputStreetName,
+        city: inputCity,
+        zip: inputZip,
+      },
+    });
+
+    //clear input fields
+    setInputName('');
+    setInputStreetName('');
+    setInputCity('');
+    setInputZip('');
+  };
+
   return (
-    <div>
-      <input>Name</input>
-      <input>Street Name</input>
-      <input>City</input>
-      <input>Zip</input>
+    <form onSubmit={handleClickNavToCheckout}>
+      <input placeholder="Name" id="name" />
+      <input placeholder="Street Address" id="streetName" />
+      <input placeholder="City" id="city" />
+      <input placeholder="Zip" id="zip" />
       <input type="checkbox">Pickup</input>
       <input type="checkbox">Delivery</input>
-    </div>
+      <Button type="submit">Next</Button>
+    </form>
   );
 }
 
