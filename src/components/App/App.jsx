9e4c59f111da196b11e1d6195394077d4d8ff30c
jessiyang1996 <jessiyang1996@gmail.com>
2024-02-10 10:09:51 -0600

    updated

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,6 @@ import './App.css';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
 // Component imports
-import Home from '../Home/Home';
 import PizzaSelection from '../PizzaSelection/PizzaSelection';
 
 function App() {
@@ -12,28 +11,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className="App-title">Prime Pizza</h1>
-
-        {/* TODO: put navigation here */}
-        <Router>
-          <div id="navigationDiv">
-            <ul>
-              <li>
-                <Link to="/">HOME</Link>
-              </li>
-              <li>
-                <Link to="/PizzaSelection">Pizza Selection</Link>
-              </li>
-            </ul>
-          </div>
-
-          {/* Routes */}
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/PizzaSelection">
-            <PizzaSelection />
-          </Route>
-        </Router>
       </header>
     </div>
   );
