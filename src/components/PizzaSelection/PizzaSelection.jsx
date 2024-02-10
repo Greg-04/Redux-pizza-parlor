747f994f39d11042dc3d9fdb0feddf7663f96091
jessiyang1996 <jessiyang1996@gmail.com>
2024-02-10 09:06:46 -0600

    created the backbone of the PizzaSelection Feature

--- src/components/PizzaSelection/PizzaSelection.jsx
+++ src/components/PizzaSelection/PizzaSelection.jsx
@@ -1,1 +1,13 @@
 // This is where clients can look at the menu and select the items they would like to purchase
+
+// imports:
+
+
+// functions:
+function PizzaSelection() {
+  // pizzaRefreshList
+  return(
+    // TODO: Display pizza menu options
+    // TODO: .map (loop through data) 
+  );
+}
