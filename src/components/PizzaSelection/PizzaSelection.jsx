a526c5a814ec1261bdce66bcc64a36d3805ce681
jessiyang1996 <jessiyang1996@gmail.com>
2024-02-10 09:10:28 -0600

    Created a separate component for the pizza item

--- src/components/PizzaSelection/PizzaSelection.jsx
+++ src/components/PizzaSelection/PizzaSelection.jsx
@@ -1,6 +1,7 @@
 // This is where clients can look at the menu and select the items they would like to purchase
 
 // imports:
+import react from 'react';
 
 
 // functions:
@@ -9,5 +10,6 @@ function PizzaSelection() {
   return(
     // TODO: Display pizza menu options
     // TODO: .map (loop through data) 
+    // TODO: 
   );
 }
