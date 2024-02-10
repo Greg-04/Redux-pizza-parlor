840cda1027af30ddb3e68bdcb3139b341b389d64
jessiyang1996 <jessiyang1996@gmail.com>
2024-02-10 09:57:11 -0600

    made a class to make the font black

--- src/components/PizzaSelection/PizzaSelection.jsx
+++ src/components/PizzaSelection/PizzaSelection.jsx
@@ -7,10 +7,11 @@ function PizzaSelection() {
   // pizzaRefreshList
   return (
     <div>
-      <h1>Step 1: Select your pizza.</h1>
-      <p> TEST TEST </p>
-      // TODO: Display pizza menu options //TODO: .map (loop through data)
-      //TODO:
+      <h1 className="blackFont">Step 1: Select your pizza.</h1>
+      <p className="blackFont">> TEST TEST </p>
+      {/* TODO: Display pizza menu options */}
+      {/* TODO: .map (loop through data) */}
+      {/* TODO: */}
     </div>
   );
 }
